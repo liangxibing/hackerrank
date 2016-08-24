4f29040ee9119b4bd297d45b371f58f17fe64a8a
XibingLiang <liangxibing@chinacloud.com.cn>
2016-08-24 16:36:01 +0800

    Add solution for "Plus Minus"

---
+++ src/com/liangxibing/algorithms/warmup/Plus_Minus.java
@@ -0,0 +1,31 @@
+package com.liangxibing.algorithms.warmup;
+
+import java.util.Scanner;
+
+/**
+ * Created by liangxibing on 2016/8/24.
+ * Problem: https://www.hackerrank.com/challenges/plus-minus
+ */
+public class Plus_Minus {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        double positive = 0;
+        double negative = 0;
+        double zeros = 0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+            if (arr[arr_i] > 0)
+                ++positive;
+            else if (arr[arr_i] < 0)
+                ++negative;
+            else
+                ++zeros;
+        }
+        System.out.println(String.format("%.6f", positive / n));
+        System.out.println(String.format("%.6f", negative / n));
+        System.out.println(String.format("%.6f", zeros / n));
+    }
+}
