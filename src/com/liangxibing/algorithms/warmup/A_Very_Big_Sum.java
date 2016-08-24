4668add9542add2d7d4dbf3d7a43b517abc01408
XibingLiang <liangxibing@chinacloud.com.cn>
2016-08-24 16:21:56 +0800

    Add solution for "A Very Big Sum"

---
+++ src/com/liangxibing/algorithms/warmup/A_Very_Big_Sum.java
@@ -0,0 +1,22 @@
+package com.liangxibing.algorithms.warmup;
+
+import java.util.Scanner;
+
+/**
+ * Created by liangxibing on 2016/8/24.
+ * Problem: https://www.hackerrank.com/challenges/a-very-big-sum
+ */
+public class A_Very_Big_Sum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long arr[] = new long[n];
+        long sum = 0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextLong();
+            sum += arr[arr_i];
+        }
+        System.out.println(sum);
+    }
+}
