865d8afa2918f90c3598f39aab8feb2939dcd311
liangxibing <simonlaung@gmail.com>
2016-08-28 19:14:27 +0800

    Add solution for "Angry Professor"

---
+++ src/com/liangxibing/algorithms/implementation/Angry_Professor.java
@@ -0,0 +1,26 @@
+package com.liangxibing.algorithms.implementation;
+
+import java.util.Scanner;
+
+/**
+ * Created by liangxibing on 2016/8/28.
+ * Problem: https://www.hackerrank.com/challenges/angry-professor
+ */
+public class Angry_Professor {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int a[] = new int[n];
+            int inTime = 0;
+            for(int a_i=0; a_i < n; a_i++){
+                a[a_i] = in.nextInt();
+                if (a[a_i] <= 0) ++inTime;
+            }
+            System.out.println(inTime < k ? "YES" : "NO");
+        }
+    }
+}
