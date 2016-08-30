bd77554081f00ccaed9a73b6e8e422a95143bd50
XibingLiang <liangxibing@chinacloud.com.cn>
2016-08-30 09:40:54 +0800

    Add the solution for Day 20 of "30 Days of Code"

---
+++ src/com/liangxibing/tutorials/thirty_days_of_code/Day_20_Sorting.java
@@ -0,0 +1,48 @@
+package com.liangxibing.tutorials.thirty_days_of_code;
+
+import java.util.Scanner;
+
+/**
+ * Created by liangxibing on 2016/8/30.
+ * Problem: https://www.hackerrank.com/challenges/30-sorting
+ */
+public class Day_20_Sorting {
+
+    private static void swap(int i, int j, int[] array) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        in.close();
+        int totalSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    swap(j, j + 1, a);
+                    numberOfSwaps++;
+                    ++totalSwaps;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        System.out.println("Array is sorted in " + totalSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n - 1]);
+    }
+}
