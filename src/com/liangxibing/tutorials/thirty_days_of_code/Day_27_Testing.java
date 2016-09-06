413da39ce2b158f9dd159fa997b1405d850e6884
liangxibing <simonlaung@gmail.com>
2016-09-06 17:00:50 +0800

    Add solution for Day 27 of "30 Days of Code"

---
+++ src/com/liangxibing/tutorials/thirty_days_of_code/Day_27_Testing.java
@@ -0,0 +1,25 @@
+package com.liangxibing.tutorials.thirty_days_of_code;
+
+import java.util.Scanner;
+
+/**
+ * Created by liangxibing on 2016/9/6.
+ * Problem: https://www.hackerrank.com/challenges/30-testing
+ */
+public class Day_27_Testing {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println(5);
+        System.out.println("4 3");
+        System.out.println("0 -3 4 2");
+        System.out.println("5 2");
+        System.out.println("0 -3 4 2 2");
+        System.out.println("3 3");
+        System.out.println("0 -3 4");
+        System.out.println("7 2");
+        System.out.println("0 -3 1 1 1 1 1");
+        System.out.println("6 3");
+        System.out.println("0 -3 4 2 1 1");
+    }
+}
