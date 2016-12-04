efa4fecaecc18a0d20956d0554cbabacb107c7b7
liangxibing <simonlaung@gmail.com>
2016-12-05 00:08:48 +1300

    improve solution for "Trees: Is This a Binary Search Tree?" in "Cracking the Coding Interview" tutorial

--- src/com/liangxibing/tutorials/cracking_the_coding_interview/Trees_Is_This_a_Binary_Search_Tree.java
+++ src/com/liangxibing/tutorials/cracking_the_coding_interview/Trees_Is_This_a_Binary_Search_Tree.java
@@ -14,36 +14,20 @@ The Node class is defined as follows:
         Node right;
      }
     */
-    private static final int MIN_VALUE = -1;
-    private static final int MAX_VALUE = 10001;
 
     boolean checkBST(Trees_Is_This_a_Binary_Search_Tree_Node root) {
-        // if root is null then return true
-        if (null == root)
-            return true;
-        // check left/right node of root
-        return check(root.left, MIN_VALUE, root.data) && check(root.right, root.data, MAX_VALUE);
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    private boolean check(Trees_Is_This_a_Binary_Search_Tree_Node node, int min, int max) {
+    private boolean checkBST(Trees_Is_This_a_Binary_Search_Tree_Node node, int min, int max) {
         // if node is null then return true
         if (null == node)
             return true;
         // check node is between min and max
-        if (node.data <= min || node.data >= max)
+        if (node.data < min || node.data > max)
             return false;
-        boolean checkLeft = true;
-        boolean checkRight = true;
-        // if left node is not null then check left node between min and node recursively
-        if (null != node.left) {
-            checkLeft = check(node.left, min, node.data);
-        }
-        // if right node is not null then check right node between node and max recursively
-        if (null != node.right) {
-            checkRight = check(node.right, node.data, max);
-        }
-        // if check result of left and right are both true then return true
-        return checkLeft && checkRight;
+        // check the left/right node recursively
+        return checkBST(node.left, min, node.data - 1) && checkBST(node.right, node.data + 1, max);
     }
 
 }
