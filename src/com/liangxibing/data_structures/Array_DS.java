5c89f8906460da067d8dba5670cdc47600ae4af8
Simon Liang <simon@parrotanalytics.com>
2018-03-22 21:52:58 +1300

    add solution for "Arrays - DS" of "Data Structures"

---
+++ src/com/liangxibing/data_structures/Array_DS.java
@@ -0,0 +1,30 @@
+package com.liangxibing.data_structures;
+
+import java.util.Scanner;
+
+/**
+ * Created by Xibing Liang on 2018/03/22.
+ * Problem: https://www.hackerrank.com/challenges/arrays-ds/problem
+ */
+public class Array_DS {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scan.nextLine().split(" ");
+
+        for (int arrItr = 0; arrItr < n; arrItr++) {
+            int arrItem = Integer.parseInt(arrItems[arrItr].trim());
+            arr[arrItr] = arrItem;
+        }
+
+        System.out.print(arr[n - 1]);
+        for (int arrItr = 1; arrItr < n; arrItr++) {
+            System.out.print(" " + arr[n - 1 - arrItr]);
+        }
+    }
+}
