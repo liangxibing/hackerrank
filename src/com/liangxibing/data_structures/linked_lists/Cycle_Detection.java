46e8068824945c9e7e896947e8ee547d78dcdf42
Simon Liang <simon@parrotanalytics.com>
2018-04-09 11:20:38 +1200

    solution for "Cycle Detection" of "Data Structures"

---
+++ src/com/liangxibing/data_structures/linked_lists/Cycle_Detection.java
@@ -0,0 +1,25 @@
+package com.liangxibing.data_structures.linked_lists;
+
+/**
+ * Created by Xibing Liang on 2018/04/09.
+ * Problem: https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+ */
+public class Cycle_Detection {
+
+    class Node {
+        int data;
+        Node next;
+    }
+
+    boolean hasCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
